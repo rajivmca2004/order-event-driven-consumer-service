8cd9587931a43d6efcefd9f9264a312a9cfabde6
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-12-17 23:14:32 +0530

    Refactor code

--- src/main/java/com/online/store/demo/ordereventdriven/stream/OrderEventConsumerStreamService.java
+++ src/main/java/com/online/store/demo/ordereventdriven/stream/OrderEventConsumerStreamService.java
@@ -8,8 +8,10 @@ import com.online.store.demo.ordereventdriven.model.Message;
 
 @Configuration
 public class OrderEventConsumerStreamService {
+	
 	@StreamListener(OrderEventConsumerStream.INBOUND)
 	public void consumeEvent(@Payload Message msg) {
+		
 		System.out.println("Inbound purchase order message consumed ==> id: " + msg.getId() + " Purchase Order message: "
 				+ msg.getData() + " bytePayload: " + msg.getBytePayload());
 	}
